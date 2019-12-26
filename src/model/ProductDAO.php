86e0231fe7cd09aec960401ad876dfc640db05bf
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-26 12:13:38 -0300

    Created methods dao

--- src/model/ProductDAO.php
+++ src/model/ProductDAO.php
@@ -1,3 +1,24 @@
 <?php
 
-?>
+class ProductDAO
+{
+    public function Create(Product $product)
+    {
+
+    }
+
+    public function Read()
+    {
+
+    }
+
+    public function Update(Product $product)
+    {
+
+    }
+
+    public function Delete($id)
+    {
+
+    }
+}
