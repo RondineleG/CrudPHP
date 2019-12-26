56bbacce5621525f0a7d256f616049a40de0b570
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-26 12:25:49 -0300

    Add  autoload composer

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CRUD PHP</title>
-    <link href="/Assets/css/main.css" rel="stylesheet">
-</head>
 
-<body>
-    <h1>Learning PHP</h1>
     <?php
-     
-    ?>
-</body>
 
-</html>
+
+    ?>
