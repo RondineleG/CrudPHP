5c840a9d424a9fb7a75e8d3b3f49265956e6cf81
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-26 12:11:01 -0300

    Creating GET an SET

--- src/model/Product.php
+++ src/model/Product.php
@@ -1,5 +1,30 @@
       
 <?php
 
+class Product
+{
+     private $Name , $Description;
+
+     
+     public function getName()
+     {
+          return $this->Name;
+     }
+
+     public function setName($Name)
+     {
+          $this->Name = $Name;
+     }
+
+     public function getDescription()
+     {
+          return $this->Description;
+     }
+     public function setDescription($Description)
+     {
+          $this->Description = $Description;
+     }
+}
+
 
 ?>
