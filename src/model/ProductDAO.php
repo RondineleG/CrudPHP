9d74e692eb2be2d5867e6e8bd282c306a844adeb
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-26 14:02:34 -0300

    Create Method

--- src/model/ProductDAO.php
+++ src/model/ProductDAO.php
@@ -6,6 +6,11 @@ class ProductDAO
 {
     public function Create(Product $product)
     {
+        $sql = "INSERT INTO product (Name, Description)VALUES(?,?)";
+        $stmt = Connection::getConnection()->prepare($sql);
+        $stmt -> bindValue(1, $product->getName());
+        $stmt -> bindValue(2, $product->getDescription());
+        $stmt->execute();
     }
 
     public function Read()
