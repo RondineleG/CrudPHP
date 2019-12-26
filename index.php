0a1016c0a611d22e70a80998f8709f30eeb57189
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-26 13:48:26 -0300

    Update connection

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 
     <?php
+
     require_once 'vendor/autoload.php';
 
-    ?>
+    $product = new src\Model\Product;
+    $product->setName("Pen");
+    $product->setDescription("My pen is very coll");
+
+    var_dump($product);
+
+    $productDAO = new \src\Model\ProductDAO();
+
+    $productDAO->Create($product);
