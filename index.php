f06a88ba3183610fb82abb1f212156e8939831ff
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-27 10:37:01 -0300

    Complet CRUD

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
  <?php
 
     require_once 'vendor/autoload.php';
-     require './src/model/Product.php';
-     require './src/model/ProductDAO.php';
 
     $product = new \src\Model\Product();
     $product->setName("Pen");
@@ -11,4 +9,19 @@
 $productDAO = new \src\Model\ProductDAO();
 
 $productDAO->Create($product);
-    
+
+$productDAO->Delete(5);
+
+$productDAO->Update($produto);
+
+$productDAO->Create($produto);
+
+$productDAO->Read();
+
+var_dump($produtoDao->Read());
+
+foreach($productDAO->read() as $produto)
+{
+    echo $product['name']."<br>".$produto['description']."<hr>";
+}
+   
